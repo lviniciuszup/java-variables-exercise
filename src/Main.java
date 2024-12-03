6b8f024d42c617a878c4e239660090b4cc11c0fc
lviniciuszup <luiz.vincius@zup.com.br>
2024-12-03 10:36:09 -0300

    Add variable declaration exercise

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Luiz Vinicius";
+        int idade = 25;
+        double altura = 1.72;
+        boolean matriculado = true;
+
+        System.out.println("Nome: "+nome  +" Idade: "+idade + " Altura: " + altura + " Matriculado: " + matriculado);
+    }
+}
